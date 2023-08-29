d680c9be08c09a760e6282963924cc3904f0325c
ManishKansana <manish.kansana.24@gmail.com>
2023-08-29 22:50:43 +0530

    added uploadthing - Refactored onboarding page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      serverActions: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "img.clerk.com",
+          port: ""
+        },
+        {
+          protocol: "https",
+          hostname: "images.clerk.dev",
+          port: ""
+        },
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+          port: ""
+        },
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+          port: ""
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig
